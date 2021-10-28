787551a6186765976365c5e88893b1715653f0b4
Tristan <tmarquise@datamarq.com>
2021-10-28 02:59:20 -0400

    Updated ssr mode and target

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  ssr: false,
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - stratbook',
